23c113ba8d24af2895dd8a53011b0a66022869b6
Christan Grant <christangrant@gmail.com>
2012-10-30 18:39:08 -0300

    Update web/util.php added function getQueryPlan

--- web/util.php
+++ web/util.php
@@ -4,4 +4,14 @@ function make_db_string() {
 	return json_decode(file_get_contents("db.json"));
 }
 
+function getQueryPlan($conn, $query) {
+	// This functions prepends "EXPLAIN " To a query and returns the results
+
+	$result = pg_query("EXPLAIN ".$query);
+
+	$arr = pg_fetch_all($result);
+
+	return $arr;
+}
+
 ?>
